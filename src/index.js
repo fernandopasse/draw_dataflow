5c8a8fa4c5f216dc2856bccb944567ef733880be
Cathoud <cathoud@hotmail.com>
2020-06-03 01:34:38 -0300

    Selecionar nodos do mesmo tipo

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import $ from './jquery-3.5.1.min';
 import contextMenus from './cytoscape-context-menus';
 import cola from 'cytoscape-cola'
 
-cytoscape.use( edgehandles );
+cytoscape.use(edgehandles);
 cytoscape.use(contextMenus, $);
 cytoscape.use(cola);
 
@@ -15,22 +15,10 @@ var cy = cytoscape({
       container: document.getElementById('cy'),
 
       layout: {
-        name: 'cola'
+        name: 'cola', fit: false
       },
 
       style: [
-        {
-          selector: '.eh-handle',
-          css: {
-            'background-color': 'red',
-            'width': 5,
-            'height': 5,
-            'shape': 'ellipse',
-            'overlay-opacity': 0,
-            'border-width': 5, // makes the handle easier to hit
-            'border-opacity': 0
-          }
-        },
         {
           selector: '.soma',
           css: {
@@ -75,12 +63,28 @@ var cy = cytoscape({
           }
         },
         {
-          selector: 'node',
+          selector: 'nodes',
           css: {
             'background-fit': 'contain',
             'border-color': '#000',
             'border-width': 1,
-            'border-opacity': 0.5
+            'border-opacity': 0.5,
+            'width': 50,
+            'height': 50
+          }
+        },
+        {
+          selector: '.eh-handle',
+          css: {
+            'background-color': 'red',
+            'width': 15,
+            'height': 15,
+            'shape': 'ellipse',
+            'overlay-opacity': 0,
+            'border-width': 5, // makes the handle easier to hit
+            'border-opacity': 0,
+            'background-color': '#e53935',
+            'border-color': '#ab000d',
           }
         },
         {
@@ -88,6 +92,13 @@ var cy = cytoscape({
           css: {
             'curve-style': 'bezier',
             'target-arrow-shape': 'triangle'
+          }
+        },
+        {
+          selector: ':selected',
+          css: {
+            'background-color': '#90caf9',
+            'border-color': '#002171',
           }
         }
       ],
@@ -102,7 +113,13 @@ var cy = cytoscape({
 const selectAllOfTheSameType = function (ele) {
   cy.elements().unselect()
   if (ele.isNode()) {
-    cy.nodes().select()
+    console.log(ele.data())
+    cy.nodes(`[type = "${ele.data('type')}"]`).select()//.addClass('selected')
+    //   console.log(node)
+    // }).select()
+    //cy.nodes().select().filter('.input').addClass('selected')
+    //cy.$(':selected').addClass('selected');
+    //console.log(cy.$(':selected'))   
   } else if (ele.isEdge()) {
     cy.edges().select()
   }
@@ -143,12 +160,13 @@ const contextMenu = cy.contextMenus({
       coreAsWell: true,
       onClickFunction: function (event) {
         var data = {
-          group: 'nodes',
+          //group: 'nodes',
           id: Math.round(Math.random() * 100),
           type: 'input'
         }
         var pos = event.position || event.cyPosition
         cy.add({
+          group: 'nodes',
           data: data,
           position: {
             x: pos.x,
@@ -165,12 +183,13 @@ const contextMenu = cy.contextMenus({
       coreAsWell: true,
       onClickFunction: function (event) {
         var data = {
-          group: 'nodes',
+          //group: 'nodes',
           id: Math.round(Math.random() * 100),
           type: 'output'
         }
         var pos = event.position || event.cyPosition
         cy.add({
+          group: 'nodes',
           data: data,
           position: {
             x: pos.x,
@@ -187,12 +206,13 @@ const contextMenu = cy.contextMenus({
       coreAsWell: true,
       onClickFunction: function (event) {
         var data = {
-          group: 'nodes',
+          //group: 'nodes',
           id: Math.round(Math.random() * 100),
           type: '+'
         }
         var pos = event.position || event.cyPosition
         cy.add({
+          group: 'nodes',
           data: data,
           position: {
             x: pos.x,
@@ -209,12 +229,13 @@ const contextMenu = cy.contextMenus({
       coreAsWell: true,
       onClickFunction: function (event) {
         var data = {
-          group: 'nodes',
+          //group: 'nodes',
           id: Math.round(Math.random() * 100),
           type: '-'
         }
         var pos = event.position || event.cyPosition
         cy.add({
+          group: 'nodes',
           data: data,
           position: {
             x: pos.x,
@@ -231,12 +252,13 @@ const contextMenu = cy.contextMenus({
       coreAsWell: true,
       onClickFunction: function (event) {
         var data = {
-          group: 'nodes',
+          //group: 'nodes',
           id: Math.round(Math.random() * 100),
           type: '*'
         }
         var pos = event.position || event.cyPosition
         cy.add({
+          group: 'nodes',
           data: data,
           position: {
             x: pos.x,
@@ -257,7 +279,7 @@ const contextMenu = cy.contextMenus({
     },
     {
       id: 'selecionar-nodos',
-      content: 'Seleciona todos os NODOS',
+      content: 'Seleciona todos os NODOS do mesmo tipo',
       tooltipText: 'Selecionar todos os NODOS',
       selector: 'node',
       onClickFunction: function (event) {
@@ -275,6 +297,34 @@ const contextMenu = cy.contextMenus({
     }
   ]
 });
+
+
+
+// var data = {
+//   group: 'nodes',
+//   id: Math.round(Math.random() * 100),
+//   type: '*'
+// }
+// var pos = event.position || event.cyPosition
+// cy.add({
+//   data: data,
+//   position: {
+//     x: pos.x,
+//     y: pos.y
+//   },
+//   classes: ['multiplicacao']
+// })
+// }
+cy.add([
+  { group: 'nodes', data: { id: 'n0', type: '+' }, position: { x: 700, y: 100 }, classes: ['soma'] },
+  { group: 'nodes', data: { id: 'n1', type: '-' }, position: { x: 800, y: 200 }, classes: ['subtracao'] },
+  { group: 'nodes', data: { id: 'n2', type: 'input' }, position: { x: 600, y: 300 }, classes: ['input'] },
+  { group: 'nodes', data: { id: 'n3', type: 'input' }, position: { x: 700, y: 400 }, classes: ['input'] },
+  { group: 'nodes', data: { id: 'n4', type: '+' }, position: { x: 700, y: 300 }, classes: ['soma'] },
+  { group: 'nodes', data: { id: 'n5', type: '-' }, position: { x: 700, y: 200 }, classes: ['subtracao'] },
+  { group: 'nodes', data: { id: 'n6', type: '+' }, position: { x: 600, y: 200 }, classes: ['soma'] },
+]);
+console.log(cy.zoom())
 
 document.getElementById('exportar').addEventListener("click", ()=> {
   console.log("okok")
