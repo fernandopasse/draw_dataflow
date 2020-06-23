d89716bd23c8afee2aafbfb1535fa77b8ae32077
Cathoud <cathoud@hotmail.com>
2020-06-23 12:30:11 -0300

    nodetypes

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-const PUBLIC_DIR = 'dist'
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const PUBLIC_DIR = 'dist';
 
 module.exports = {
   devServer: {
     contentBase: path.join(__dirname, PUBLIC_DIR),
     port: 3340,
   },
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'js', 'index.js'),
   mode: 'development',
   module: {
     rules: [
@@ -40,15 +41,15 @@ module.exports = {
           {
             loader: 'css-loader', // translates CSS into CommonJS modules
           },
-          {
-            loader: 'postcss-loader', // Run post css actions
-            options: {
-              plugins: function () {
-                // post css plugins, can be exported to postcss.config.js
-                return [require('precss'), require('autoprefixer')];
-              },
-            },
-          },
+          // {
+          //   loader: 'postcss-loader', // Run post css actions
+          //   options: {
+          //     plugins: function () {
+          //       // post css plugins, can be exported to postcss.config.js
+          //       return [require('precss'), require('autoprefixer')];
+          //     },
+          //   },
+          // },
           {
             loader: 'sass-loader', // compiles Sass to CSS
           },
@@ -58,17 +59,30 @@ module.exports = {
   },
   output: {
     filename: '[name]-[hash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, PUBLIC_DIR),
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: path.join(__dirname, PUBLIC_DIR, 'index.html'),
+      template: path.join(__dirname, 'src', 'index.html'),
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'assets/**/*.png',
+          context: path.resolve(__dirname, 'src'),
+        },
+        {
+          from: 'assets/**/*.json',
+          context: path.resolve(__dirname, 'src'),
+        },
+      ],
+    }),
+    new CleanWebpackPlugin(),
   ],
   // externals: {
   //   jquery: 'jquery'
